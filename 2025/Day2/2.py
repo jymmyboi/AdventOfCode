b7fc91d9a2a2666e1d05380fc931d2f20196b1ee
James Seton <jamesseton6@gmail.com>
2025-12-02 17:06:53 +1030

    Day 2 Part 2 done

--- 2025/Day2/2.py
+++ 2025/Day2/2.py
@@ -2,23 +2,32 @@ with open("2025/Day2/2.txt") as file:
     raw_input = file.read()
     input = raw_input.split(',')
     
-total = 0
+part_1 = 0
+part_2 = 0
 
-def repeat_check(id_range):
+def part_1_repeat_check(id_range):
     range_total = 0
     range_values = id_range.split('-')
-    print(range_values)
     for number in range(int(range_values[0]), (int(range_values[1]) + 1)):
         num_string = str(number)
         firstpart, secondpart = num_string[:len(num_string)//2], num_string[len(num_string)//2:]
         if firstpart == secondpart:
             range_total += int(number)
-            print(number)
-    print(f"Range total {range_total}")
+    return range_total
+
+def part_2_repeat_check(id_range):
+    range_total = 0
+    range_values = id_range.split('-')
+    for number in range(int(range_values[0]), (int(range_values[1]) + 1)):
+        repeats = (str(number) + str(number)).find(str(number), 1) != len(str(number))
+        if repeats:
+            range_total += int(number)
     return range_total
     
 
 for number in input:
-    total += repeat_check(number)
+    part_1 += part_1_repeat_check(number)
+    part_2 += part_2_repeat_check(number)
 
-print(total)
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
