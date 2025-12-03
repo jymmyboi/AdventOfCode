2603e9377d403db6c58474cf75fa995f253d6c92
James Seton <jamesseton6@gmail.com>
2025-12-03 23:11:41 +1030

    Day 3 Part 2 finished

--- 2025/Day3/3.py
+++ 2025/Day3/3.py
@@ -13,19 +13,33 @@ def highest_check_part_1(line):
 def highest_check_part_2(line, index_amt):
     num_list = [int(i) for i in str(line).strip()]
     digit_list = []
-    max_index = num_list.index(max(num_list[:(index_amt - 1)]))
-    digit_list.append(num_list[max_index])
-    print(digit_list)
+    index_start = 0
     for i in range(index_amt):
-        print(digit_list)
-        max_index = num_list.index(max(num_list[max_index+1:((index_amt-1) - i) if (index_amt-1) > 0 else 1]))
-        digit_list.append(num_list[max_index])
-        
+        max_i, max_num = highest_check(num_list, index_start, (index_amt-1) - i)
+        index_start += max_i + 1
+        digit_list.append(max_num)
+    print(''.join(str(item) for item in digit_list))
+    return int(''.join(str(item) for item in digit_list))
 
-total = 0
+        
+def highest_check(lst, start, end):
+    temp_highest = 0
+    highest_index = None
+    list_check = lst[start:-end] if end != 0 else lst[start:]
+
+    for i, value in enumerate(list_check):
+        if value > temp_highest:
+            temp_highest = value
+            highest_index = i
+    return highest_index, temp_highest
+
+
+part_1_total = 0
+part_2_total = 0
 
 for line in raw_input:
-    total += int(highest_check_part_1(line))
-    highest_check_part_2(line, 12)
+    part_1_total += int(highest_check_part_1(line))
+    part_2_total += highest_check_part_2(line, 12)
 
-print(f"Part 1: {total}")
+print(f"Part 1: {part_1_total}")
+print(f"Part 2: {part_2_total}")
